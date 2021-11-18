6dc13e1f3633ac6a52f64f586a3cfd5b821bff58
matheoleger <71370253+matheoleger@users.noreply.github.com>
2021-11-18 16:37:08 +0100

    Change idle animation and system
    I added 4 directionnal direction for idle (with Blend Tree in the animator).
    I added 2 properties for animator : previousHorizontal and previousVertical (I used it to keep the previous value of the direction).

--- Assets/Scripts/Characters/Player.cs
+++ Assets/Scripts/Characters/Player.cs
@@ -22,6 +22,12 @@ public class Player : Characters
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if(movement.x != 0 && movement.y == 0 || movement.x == 0 && movement.y != 0)
+        {
+            animator.SetFloat("PreviousHorizontal", movement.x);
+            animator.SetFloat("PreviousVertical", movement.y);
+        }
     }
 
 }
