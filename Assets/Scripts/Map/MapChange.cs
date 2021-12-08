974b2d3fb174dfbf2baf71dcbf3e604e3a018be7
LBROCHARD <73283488+LBROCHARD@users.noreply.github.com>
2021-12-08 11:16:01 +0100

    Map
    added the first few test maps as well as trantitions between them and tilemap systèm

--- Assets/Scripts/Map/MapChange.cs
+++ Assets/Scripts/Map/MapChange.cs
@@ -5,16 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class MapChange : MonoBehaviour
 {
-    public Collider2D colliderName;
+    public Collider2D colliderName; //let the script in unity have an argument of wich trigger box is triggered
 
-    public int Scenenumber;
+    public int Scenenumber; //let the script in unity have an argument of wich scene should you launch
 
+    public Animator transition; // let the script in unity select an animation
+    public float transitionTime = 1f ;// let the script in unity select a time of animation
 
-    void OnTriggerEnter2D(Collider2D colliderName) 
+    void OnTriggerEnter2D(Collider2D colliderName) //to call on triggerbox to change scene
     {
         if(colliderName.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene(Scenenumber); //1 is the build order it could be 1065 for you if you have that many scenes
+            LoadLevel(Scenenumber); 
         }
     }
+
+    public void LoadLevel(int SceneToLoad){ //public to call it in other scripts (like just to change scene for x reasons...)
+
+        StartCoroutine(LoadTransitionLevel(SceneToLoad)); 
+
+    }
+
+    IEnumerator LoadTransitionLevel(int SceneToLoad){
+
+        transition.SetTrigger("quit_trigger");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(SceneToLoad); 
+    }
+
+
+
 }
