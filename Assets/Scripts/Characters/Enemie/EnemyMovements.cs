142374cfa050cc4b2bac096823dcaa9d0c36598c
matheoleger <71370253+matheoleger@users.noreply.github.com>
2022-01-19 16:54:58 +0100

    Fix + refacto of movement of ennemies
    After, we can update the movement (because, now, the movement is only on axes x or y).

--- Assets/Scripts/Characters/Enemie/EnemyMovements.cs
+++ Assets/Scripts/Characters/Enemie/EnemyMovements.cs
@@ -6,37 +6,34 @@ using System.Threading.Tasks;
 using UnityEngine;
 
 
-namespace Assets.Scripts.Characters.Enemie
+public class EnemyMovements : Ennemies
 {
-    internal class EnemyMovements : Ennemies
+    public void moveEnemy(Vector3 p )
     {
-        public void moveEnemy(Vector3 p )
+        //Debug.Log(Player.Instance.transform.position.x - transform.position.x);
+        //movement = new Vector2(Player.transform.position.x, Player.transform.position.y);
+        //rb.MovePosition(rb.position + (movement * speedMovement * Time.fixedDeltaTime));
+        //Debug.Log(Player.Instance.transform.position.x - transform.position.x);
+        //Debug.Log(Player.Instance.transform.position.y - transform.position.y);
+
+        if (rb.transform.position.x - transform.position.x == 0)
         {
-            //Debug.Log(Player.Instance.transform.position.x - transform.position.x);
-            //movement = new Vector2(Player.transform.position.x, Player.transform.position.y);
-            //rb.MovePosition(rb.position + (movement * speedMovement * Time.fixedDeltaTime));
-            //Debug.Log(Player.Instance.transform.position.x - transform.position.x);
-            //Debug.Log(Player.Instance.transform.position.y - transform.position.y);
+            animator.SetFloat("Horizontal", p.x);
 
-            if (rb.transform.position.x - transform.position.x == 0)
-            {
-                animator.SetFloat("Horizontal", p.x);
-
-                transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, p.x), speedMovement * Time.deltaTime);
-            }
-            else
-            {
-                animator.SetFloat("Vertical", rb.transform.position.y);
-
-                transform.position = Vector2.MoveTowards(transform.position, new Vector2(p.x, transform.position.y), speedMovement * Time.deltaTime);
-            }
-
-
-            //if (Player.Instance.transform.position.x - GameObject.Find("Enemy").transform.position.x > 0 && Player.Instance.transform.position.y - GameObject.Find("Enemy").transform.position.y > 0)
-            //{
-            //    movement = new Vector2(0.5f, 0.5f);
-            //    rb.MovePosition(rb.position + (speedMovement * Time.fixedDeltaTime * movement));
-            //}
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, p.x), speedMovement * Time.deltaTime);
         }
+        else
+        {
+            animator.SetFloat("Vertical", rb.transform.position.y);
+
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(p.x, transform.position.y), speedMovement * Time.deltaTime);
+        }
+
+
+        //if (Player.Instance.transform.position.x - GameObject.Find("Enemy").transform.position.x > 0 && Player.Instance.transform.position.y - GameObject.Find("Enemy").transform.position.y > 0)
+        //{
+        //    movement = new Vector2(0.5f, 0.5f);
+        //    rb.MovePosition(rb.position + (speedMovement * Time.fixedDeltaTime * movement));
+        //}
     }
 }
--- Assets/Scripts/Characters/Enemie/Ennemies.cs
+++ Assets/Scripts/Characters/Enemie/Ennemies.cs
@@ -1,4 +1,3 @@
-using Assets.Scripts.Characters.Enemie;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,16 +11,15 @@ public class Ennemies : Characters
 
     }
 
-    private EnemyMovements EnemyMovements;
     public Animator animator;
+    public float targetRange = 5f;
     private bool findTarget;
-    private Vector3 moveDirection;
+    private Vector2 moveDirection;
     private State state;
 
     private void Awake()
     {
-        EnemyMovements = rb.GetComponent<EnemyMovements>();
-        Debug.Log(EnemyMovements);
+        Debug.Log(rb.gameObject);
         state = State.Waiting;
     }
 
@@ -42,7 +40,7 @@ public class Ennemies : Characters
                 FindTarget();
                 break;
             case State.chaseTarget:
-                EnemyMovements.moveEnemy(rb.transform.position);
+                moveEnemy(rb.transform.position); //RigidBody du player
                 break;
             default:    
                 break;
@@ -50,35 +48,56 @@ public class Ennemies : Characters
 
     }
 
-    public void TakeDamage(int damage)
-    {
-
-        if (health > 0)
-        {
-            health -= damage;
-        }
-
-
-        if (health <= 0)
-        {
-            Die();
-        }
-    }
-
-    void Die()
-    {
-        Destroy(gameObject);
-    }
-
-  
-
     private void FindTarget()
     {
-        float targetRange = 5f;
-        if (Vector3.Distance(transform.position, rb.transform.position) < targetRange)
+        if (Vector2.Distance(transform.position, rb.transform.position) < targetRange)
         {
             state = State.chaseTarget;
         }
+    }
+
+    public void moveEnemy(Vector2 p)
+    {
+        //Debug.Log(Player.Instance.transform.position.x - transform.position.x);
+        //movement = new Vector2(Player.transform.position.x, Player.transform.position.y);
+        //rb.MovePosition(rb.position + (movement * speedMovement * Time.fixedDeltaTime));
+        //Debug.Log(Player.Instance.transform.position.x - transform.position.x);
+        //Debug.Log(Player.Instance.transform.position.y - transform.position.y);
+
+        if (p.x - transform.position.x == 0)
+        {
+            animator.SetFloat("Horizontal", p.x);
+
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, p.y), speedMovement * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetFloat("Vertical", p.y);
+
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(p.x, transform.position.y), speedMovement * Time.deltaTime);
+        }
+
+        //if (p.x > transform.position.x)
+        //{
+        //    movement.x = 1;
+        //} else if (p.x < transform.position.x)
+        //{
+        //    movement.x = -1;
+        //} else if (p.y > transform.position.y)
+        //{
+        //    movement.y = 1;
+        //}
+        //else if (p.y < transform.position.y)
+        //{
+        //    movement.y = -1;
+        //}
+
+
+        //if (Player.Instance.transform.position.x - GameObject.Find("Enemy").transform.position.x > 0 && Player.Instance.transform.position.y - GameObject.Find("Enemy").transform.position.y > 0)
+        //{
+        //    movement = new Vector2(0.5f, 0.5f);
+        //    rb.MovePosition(rb.position + (speedMovement * Time.fixedDeltaTime * movement));
+        //}
     }
 
     //void OnTriggerExit2D(Collider2D other)
