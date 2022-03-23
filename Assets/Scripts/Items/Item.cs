503d42abb4020493c340fd3b58d6bad9cb8c21c1
matheoleger <71370253+matheoleger@users.noreply.github.com>
2022-03-23 08:29:44 +0100

    feat(Item): Added type of item for verification of what is the item
    The possible type of item are: useless, armor, weapon, fishing rod

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
 public class Item : ScriptableObject
 {
+
+    public enum TypeOfItem
+    {
+        useless,
+        weapon,
+        fishingRod,
+        armor
+    }
+
     public int itemsAmount = 1;
     new public string name = "New Item";
     public Sprite icon = null;
     public bool isDefaultItem = false;
+
+    public TypeOfItem typeOfItem = TypeOfItem.useless;
 
     public void OnEnable()
     {
