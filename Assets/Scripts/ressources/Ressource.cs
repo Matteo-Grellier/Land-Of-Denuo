91e018c874ab232999e487e0aa64024ba55073a6
LBROCHARD <73283488+LBROCHARD@users.noreply.github.com>
2022-04-06 16:52:25 +0200

    [Ressources.unity, Assets/Prefabs/Ressources/, Ressource.cs] added prefab and dcript for ressources objects

---
+++ Assets/Scripts/ressources/Ressource.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ressource : MonoBehaviour
+{
+    private bool playerInCollider = false;
+    private bool canCut = true;
+
+    private float nextTime = 0.0f;
+
+    public int hp = 100;
+    public int timeToDestroy = 20;
+
+    public GameObject item;
+    public GameObject leftOvers;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            playerInCollider = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            playerInCollider = false;
+        }
+    }
+
+    void Update()
+    {
+        if (hp <= 0)
+        {
+            // Instantiate(item, transform.position, transform.rotation);
+            Instantiate(leftOvers, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && playerInCollider == true && canCut == true)
+        {
+            ShakeTree(Time.time);
+            hp = hp - timeToDestroy;
+
+            int chance = Random.Range(1, 4);
+            if (chance == 1 || chance == 2)
+            {
+                PopItem();
+            }
+        }
+
+        if (Time.time >= nextTime && canCut == false)
+        {
+            transform.position = new Vector2(transform.position.x - 0.1f, transform.position.y);
+            canCut = true;
+        }
+    }
+
+    void ShakeTree(float time)
+    {
+        transform.position = new Vector2(transform.position.x + 0.1f, transform.position.y);
+        nextTime = time + 0.05f;
+        canCut = false;
+    }
+
+    void PopItem()
+    {
+        GameObject bob = Instantiate(item, transform.position, transform.rotation);
+        bob.transform.position =  (Vector2)transform.position +(Random.insideUnitCircle * 3  );
+    }
+
+
+}
